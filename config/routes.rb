8ca26947123dc540657f5e24698ffe7bab5b3c56
Keon Choi <keonch91@gmail.com>
2018-03-06 16:49:00 -0500

    Setup frontend user actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:show, :create, :update]
+    resource :session, only: [:create, :destroy]
   end
 
   root to: 'static_pages#root'
