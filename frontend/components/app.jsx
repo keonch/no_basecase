024f776c8bebb07009360cd0898bec3028bfeae4
Keon Choi <keonch91@gmail.com>
2018-03-06 23:46:05 -0500

    Setup nav bar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import NavBar from './nav/nav_bar_container';
 
+//test
+import SessionForm from './session/session_form';
+//TODO FIX SESSION FORMS --> LOGIN FORM CONTAINER AND SIGNUP FORM CONTAINER
+
 const App = () => (
   <div>
     <header>
       <NavBar />
     </header>
+
+
+
+
+
+
+    <SessionForm />
+
+
+
+
+
+
   </div>
 );
 
