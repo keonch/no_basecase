6aa3b4295c77e33ca403db56904477e5573cce82
Keon Choi <keonch91@gmail.com>
2018-03-09 18:53:01 -0500

    Work on questions

--- frontend/reducers/questions/questions_reducer.js
+++ frontend/reducers/questions/questions_reducer.js
@@ -10,9 +10,16 @@ const questionsReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_ALL_QUESTIONS:
+      return merge({}, oldState, action.questions)
     case RECEIVE_QUESTION:
+      return action.question;
     case REMOVE_QUESTION:
+      const newState = oldState;
+      delete newState[action.question.id]
+      return newState;
     default:
       return oldState;
   }
 }
+
+export default questionsReducer;
