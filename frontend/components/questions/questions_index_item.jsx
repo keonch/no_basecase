168f92e552def80e14d1aa38a87340ff87177434
Keon Choi <keonch91@gmail.com>
2018-03-10 22:23:50 -0500

    Update questions index and css

--- frontend/components/questions/questions_index_item.jsx
+++ frontend/components/questions/questions_index_item.jsx
@@ -4,14 +4,18 @@ import { Link } from 'react-router-dom';
 import UserTag from '../users/user_tag';
 
 const QuestionsIndexItem = (props) => {
-  
+
   return (
-    <div>
-      <p>Vote#</p>
-      <p>answer#</p>
-      <Link to={`/questions/${props.question.id}`}>{props.question.title}</Link>
-      <div>{props.question.body}</div>
-      <UserTag user={props.author} />
+    <div className='question-item'>
+      <div className='question-stats'>
+        <p>Vote#</p>
+        <p>answer#</p>
+      </div>
+      <div className='question-summary'>
+        <Link className='question-title' to={`/questions/${props.question.id}`}>{props.question.title}</Link>
+        <div className='question-body-trunk'>{props.question.body}</div>
+        <UserTag user={props.author} />
+      </div>
     </div>
   );
 };
