12ef6e4418e0cdb369b741aef16603a4f2e48fd4
Keon Choi <keonch91@gmail.com>
2018-03-20 20:59:12 -0400

    Fix logger

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,48 @@
-const path = require('path');
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/no_base_case.jsx',
+  entry: "./frontend/no_base_case.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
+  plugins: plugins,
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['env', 'react']
+          presets: ['react', 'env']
         }
       }
     ]
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
