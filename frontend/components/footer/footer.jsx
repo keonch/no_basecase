af7927884c0f68032ea6e690bde06ae7a1a2eb84
Keon Choi <keonch91@gmail.com>
2018-04-12 13:02:39 -0400

    Add links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className='footer-content'>
-        <p className='footer-links'>No Basecase</p>
-        <a href='https://github.com/keonch'><p className='footer-links'>GitHub</p></a>
-        <a href='https://www.linkedin.com/in/keon-choi/'><p className='footer-links'>LinkedIn</p></a>
-        <a href='http://keonch.me/'><p className='footer-links'>Portfolio</p></a>
+      <div className='footer-column col1'>
+        <Link to='/' className='footer-links'>
+          <img className='footer-icon' src={ window.logoLoggedIn } />
+          <span className='footer-text'>No Basecase by Keon Choi</span>
+        </Link>
+        <a  className='footer-links' href='http://keonch.me/'>
+          <img className='footer-icon' src={ window.portfolio } />
+          <span className='footer-text'>Portfolio</span>
+        </a>
+        <a  className='footer-links' href='https://www.linkedin.com/in/keon-choi/'>
+          <img className='footer-icon' src={ window.linkedin } />
+          <span className='footer-text'>LinkedIn</span>
+        </a>
+        <a  className='footer-links' href='https://github.com/keonch'>
+          <img className='footer-icon' src={ window.github } />
+          <span className='footer-text'>GitHub</span>
+        </a>
+      </div>
     </div>
   );
 };
