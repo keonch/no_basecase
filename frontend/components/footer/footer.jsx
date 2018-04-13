e2355f25f30c2a8b9fd0b530e6dbc14180c33cc7
Keon Choi <keonch91@gmail.com>
2018-04-12 22:25:34 -0400

    Add footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,23 +4,42 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className='footer-content'>
+      <Link to='/' className='footer-logo'>
+        <img className='footer-icon logo' src={ window.logoLoggedIn } />
+      </Link>
+
       <div className='footer-column col1'>
-        <Link to='/' className='footer-links'>
-          <img className='footer-icon' src={ window.logoLoggedIn } />
-          <span className='footer-text'>No Basecase by Keon Choi</span>
-        </Link>
-        <a  className='footer-links' href='http://keonch.me/'>
+        <Link to='/' className='footer-links maincat'>No Basecase</Link>
+        <Link to='/questions' className='footer-links subcat'>Questions</Link>
+        <Link to='/jobs' className='footer-links subcat'>Developer Jobs</Link>
+        <Link to='/tags' className='footer-links subcat'>Tags</Link>
+        <Link to='/users' className='footer-links subcat'>Users</Link>
+      </div>
+
+      <div className='footer-column col2'>
+        <div className='maincat'>Profile Links</div>
+        <a  className='footer-links subcat' href='http://keonch.me/'>
           <img className='footer-icon' src={ window.portfolio } />
-          <span className='footer-text'>Portfolio</span>
+          Portfolio
         </a>
-        <a  className='footer-links' href='https://www.linkedin.com/in/keon-choi/'>
+        <a  className='footer-links subcat' href='https://www.linkedin.com/in/keon-choi/'>
           <img className='footer-icon' src={ window.linkedin } />
-          <span className='footer-text'>LinkedIn</span>
+          LinkedIn
         </a>
-        <a  className='footer-links' href='https://github.com/keonch'>
+        <a  className='footer-links subcat' href='https://github.com/keonch'>
           <img className='footer-icon' src={ window.github } />
-          <span className='footer-text'>GitHub</span>
+          GitHub
         </a>
+        <a  className='footer-links subcat' href={ window.resume }>
+          <img className='footer-icon' src={ window.folder } />
+          Resume
+        </a>
+      </div>
+
+      <div className='footer-column col3'>
+        <div className='maincat'>Contact</div>
+        <a href='mailto:keonch91@gmail.com' className='footer-links subcat'>keonch91@gmail.com</a>
+        <div className='subcat'>646-520-7420</div>
       </div>
     </div>
   );
