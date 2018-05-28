7b58f836165680a83521cba89c573ec4554af422
Keon Choi <keonch91@gmail.com>
2018-05-28 01:27:07 -0400

    Change create session view

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,7 +9,7 @@ class Api::SessionsController < ApplicationController
     )
     if @user
       login(@user)
-      render "api/users/show"
+      render "api/users/session"
     else
       render json: ['The email or password is incorrect.'], status: 401
     end
--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -2,65 +2,64 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import SearchContainer from './search_container';
 
-const Nav = (props) => {
-
-  const loggedIn = !!props.currentUser;
-  // <div className='menu-items'>
-  //   <i className="fas fa-inbox"></i>
-  //   <i className="fas fa-trophy"></i>
-  //   <i className="fas fa-question-circle"></i>
-  //   <i className="fab fa-stack-exchange"></i>
-  // </div>
-  return (
-    <div className='nav'>
-      {
-        loggedIn ?
-        <Link to='/' >
+export default class Nav extends React.Component {
+  render() {
+    const loggedIn = !!this.props.currentUser;
+    // <div className='menu-items'>
+    //   <i className="fas fa-inbox"></i>
+    //   <i className="fas fa-trophy"></i>
+    //   <i className="fas fa-question-circle"></i>
+    //   <i className="fab fa-stack-exchange"></i>
+    // </div>
+    return (
+      <div className='nav'>
+        {
+          loggedIn ?
+          <Link to='/' >
           <img className='logoLoggedIn' src={ window.logoLoggedIn } />
         </Link>
         :
         <Link to='/' >
-          <img className='logo' src={ window.logo } />
-        </Link>
-      }
+        <img className='logo' src={ window.logo } />
+      </Link>
+    }
 
-      <div className='links'>
-        <NavLink to='/questions' className='navlink' >Questions</NavLink>
+    <div className='links'>
+      <NavLink to='/questions' className='navlink' >Questions</NavLink>
 
-        <div className='search-bar'>
-          <SearchContainer />
-        </div>
-      </div>
-
-      <div className='user-items'>
-        <div>
-          {
-            loggedIn ?
-              <Link to={`/users/${props.currentUser.id}`}>
-                <img className='user-nav-img' src={window.profile} />
-              </Link>
-            :
-              ''
-          }
-
-        </div>
-
-
-
-        <div className='session'>
-          {
-            loggedIn ?
-            <button className='logout' onClick={() => props.logout(props.currentUser)}>Logout</button>
-            :
-            <div className='session-btn'>
-              <Link to='/login'><button className='login'>Log In</button></Link>
-              <Link to='/signup'><button className='signup'>Sign Up</button></Link>
-            </div>
-          }
-        </div>
+      <div className='search-bar'>
+        <SearchContainer />
       </div>
     </div>
-  );
-};
 
-export default Nav;
+    <div className='user-items'>
+      <div>
+        {
+          loggedIn ?
+          <Link to={`/users/${this.props.currentUser.id}`}>
+          <img className='user-nav-img' src={window.profile} />
+        </Link>
+        :
+        ''
+      }
+
+    </div>
+
+
+
+    <div className='session'>
+      {
+        loggedIn ?
+        <button className='logout' onClick={() => this.props.logout(this.props.currentUser)}>Logout</button>
+        :
+        <div className='session-btn'>
+          <Link to='/login'><button className='login'>Log In</button></Link>
+          <Link to='/signup'><button className='signup'>Sign Up</button></Link>
+        </div>
+      }
+    </div>
+  </div>
+</div>
+);
+  }
+};
