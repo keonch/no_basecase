69590f65ca916465224484861bd532360e257da1
Keon Choi <keonch91@gmail.com>
2018-07-05 16:35:08 -0400

    setup questions controller

---
+++ app/controllers/api/questions_controller.rb
@@ -0,0 +1,56 @@
+class Api::QuestionsController < ApplicationController
+  before_action :require_logged_in!, only: [:create, :update, :destroy]
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.author_id = current_user.id
+    if @question.save
+      render :show
+    else
+      render json: @question.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question
+      if @question.author_id == current_user.id
+        if @question.update(question_params)
+          render :show
+        else
+          render json: @question.errors.full_messages, status: 422
+        end
+      else
+        render json: ["Forbidden"], status: 403
+      end
+    else
+      render json: ["Not Found"], status: 404
+    end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    if question && question.author_id == current_user.id
+      question.destroy
+      render json: question.id
+    elsif question && question.author_id != current_user.id
+      render json: ["Forbidden"], status: 403
+    else
+      render json: ["Not Found"], status: 404
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
   validates :session_token, :email, uniqueness: true
   validates :email, email: true
 
+  has_many :answers,
+    class_name: :Answer,
+    foreign_key: :author_id
+
+  has_many :questions,
+    class_name: :Question,
+    foreign_key: :author_id
+
   attr_reader :password
 
   def self.generate_session_token
