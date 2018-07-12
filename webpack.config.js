9851c13e5c0119f22a63801ba6ef942a79e5b70a
Keon Choi <keonch91@gmail.com>
2018-07-12 12:55:13 -0400

    change uglifier gem version

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,6 @@
 const path = require('path');
 const webpack = require("webpack");
 
-// var plugins = [];
-// var devPlugins = [];
-//
-// var prodPlugins = [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }),
-//   new config.optimization.minimize({
-//     compress: {
-//       warnings: true
-//     }
-//   })
-// ];
-//
-// plugins = plugins.concat(
-//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// )
-
 module.exports = {
   context: __dirname,
   entry: './frontend/no_basecase.jsx',
